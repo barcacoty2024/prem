fbc34ff5c782b108ec52d4fb433ebfb3cff1da30
barcacoty2024 <158706333+barcacoty2024@users.noreply.github.com>
2024-07-18 05:51:41 +0700

    button.py

---
+++ PyroUbot/modules/button.py
@@ -0,0 +1,22 @@
+from PyroUbot import *
+
+__MODULE__ = "button"
+__HELP__ = f"""
+ <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ʙᴜᴛᴛᴏɴ 』</b>
+
+<b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}button</code> Teks ~ Button Teks|Button Link.
+<b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> Untuk membuat teks menjadi button.
+
+• Contoh : Google ~ Klik Disini|google.com
+"""
+
+
+@PY.UBOT("button")
+async def _(client, message):
+    await cmd_button(client, message)
+
+
+@PY.INLINE("^get_button")
+@INLINE.QUERY
+async def _(client, inline_query):
+    await inline_button(client, inline_query)
